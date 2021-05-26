f4d5231bd5a05b8f459a8733cd9a41ceab90abd9
elena <elen1935@stud.kea.dk>
2021-05-26 14:35:58 +0200

    payment draft works

--- src/Paymentform.js
+++ src/Paymentform.js
@@ -1,5 +1,101 @@
 import React from "react";
 import "./App.scss";
+
+import { useState, useEffect, useRef } from "react";
+import { Button, Input } from "antd";
+import InputMask from "react-input-mask";
+import Cleave from "cleave.js/react";
+//import ReactCardFlip from "react-card-flip";
+
+import "antd/dist/antd.css";
+
+export default function Paymentform() {
+  const [name, setName] = useState("");
+  const [cardnumber, setCardnumber] = useState("");
+  const [cardnumber2, setCardnumber2] = useState("");
+  const [monthYear, setMonthYear] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  //const [submitted, setSubmitted] = useState(false);
+
+  const form = useRef(null);
+
+  useEffect(() => {
+    const isCreditCardValid = cardnumber.replaceAll(" ", "").length === 16;
+    const isMonthYearValid = monthYear.replace("/", "").length === 4;
+    setIsValid(
+      form.current.checkValidity() && isMonthYearValid && isCreditCardValid
+    );
+  }, [name, cardnumber, monthYear, cardnumber2]);
+
+/*   function onSubmit(e) {
+    e.preventDefault();
+    setSubmitted(true);
+  } */
+
+  return (
+    <div className="Paymentform">
+        <form ref={form}>
+          
+          <div className="form-control">
+            <label htmlFor="name">Name</label>
+            <Input
+              id="name"
+              type="text"
+              required
+              minLength="2"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="cc2">Credit Card 2</label>
+            <Cleave
+              id="cc2"
+              className="ant-input"
+              options={{
+                blocks: [4, 4, 4, 4],
+                delimiter: " ",
+                numericOnly: true
+              }}
+              onChange={(e) => setCardnumber2(e.target.rawValue)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="cardnumber">Card number</label>
+            <InputMask
+              mask="9999 9999 9999 9999"
+              value={cardnumber}
+              maskChar=""
+              className="ant-input"
+              onChange={(e) => setCardnumber(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="monthyear">Month/Year</label>
+            <Input
+              mask="99/99"
+              maskChar=""
+              className="ant-input"
+              required
+              value={monthYear}
+              onChange={(e) => setMonthYear(e.target.value)}
+              minLength="17"
+            ></Input>
+          </div>
+          <Button type="primary" htmlType="submit" disabled={!isValid}>
+            Submit
+          </Button>
+        </form>
+
+    </div>
+  );
+}
+
+export { Paymentform };
+
+/* import React from "react";
+import "./App.scss";
 import 'antd/dist/antd.css';
 import { Form, Input, Button, Space } from 'antd';
 
@@ -20,7 +116,7 @@ import { Form, Input, Button, Space } from 'antd';
             noStyle
             rules={[{ required: true, message: 'Enter a valid card number' }]}
           >
-            <Input style={{ width: 300 }} placeholder="Card number" />
+            <Input type="text" style={{ width: 300 }} minLength="16" maxLength="16" pattern="[0-9]{16}" placeholder="Card number" />
           </Form.Item>
         </Space>
       </Form.Item>
@@ -32,7 +128,7 @@ import { Form, Input, Button, Space } from 'antd';
             noStyle
             rules={[{ required: true, message: 'Enter your name exactly as it is written in your card' }]}
           >
-            <Input style={{ width: 300 }} placeholder="Name on card" />
+            <Input style={{ width: 300 }} minLength="2" maxLength="26" placeholder="Name on card" />
           </Form.Item>
         </Space>
       </Form.Item>
@@ -42,7 +138,7 @@ import { Form, Input, Button, Space } from 'antd';
           name="expirationdate"
           rules={[{ required: true, message: 'Enter a valid card expiration date' }]}
         >
-         <Input style={{ width: 150 }} placeholder="(MM/YY)" />
+         <Input style={{ width: 150 }} minLength="5" maxLength="5" placeholder="(MM/YY)" />
         </Form.Item>
       </Form.Item>
 
@@ -53,7 +149,7 @@ import { Form, Input, Button, Space } from 'antd';
             noStyle
             rules={[{ required: true, message: 'Enter the 3-digit security code on the back of your card' }]}
           >
-            <Input style={{ width: 100 }} placeholder="CVV" />
+            <Input style={{ width: 100 }} minLength="3" maxLength="3" placeholder="CVV" />
           </Form.Item>
         </Space>
       </Form.Item>
@@ -66,4 +162,4 @@ import { Form, Input, Button, Space } from 'antd';
   );
 };
 
-export { Paymentform }; 
+export { Paymentform };  */
